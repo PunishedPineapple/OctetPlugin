c3e17c1f5825837d8e741e4e47271d133dd792e2
Anon User <anon@user.org>
2019-05-14 01:07:28 -0500

    Updated Documentation

--- OctetComputer.cs
+++ OctetComputer.cs
@@ -1,20 +1,29 @@
-﻿//	Note, this software is largely derived from Tarutella's Titan Gaol Plugin (https://github.com/Tarutella/titan-jail-plugin).  As there was no visible license
-//	on that software, the following license applies unless Tarutella retroactively changes the license on their software.
-//
-//
-//	The MIT License
-//
-//	Copyright 2019 Punished Pineapple
-//
-//	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
-//	in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//	copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-//	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-//	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-//	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+﻿//	Note, this software is largely derived from Tarutella's Titan Gaol
+//	Plugin (https://github.com/Tarutella/titan-jail-plugin).  As there
+//	was no visible license on that software, the following license applies
+//	unless Tarutella retroactively changes the license on their software.
+
+//	MIT License
+//	
+//	Copyright (c) 2019 PunishedPineapple
+//	
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//	
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//	
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
 
 using System;
 using System.Collections.Generic;
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# OctetPlugin
-ACT plugin to compute and Say Twintania dive marker during Grand Octet.
+# Grand Octet Plugin
+This is an ACT plugin that determines who will have the Twintania marker during Grand Octet, and uses TTS to say the name of that player.
+
+Download The Plugin DLL:\
+https://github.com/PunishedPineapple/OctetPlugin/releases/latest
+
+## Important:
+This is a work in progress, and has not been verified to work correctly during the actual fight.
+
+## Steps:
+1. Copy the plugin DLL to the desired location.
+2. Add the plugin to ACT.
+3. Fill the names of your party members into the list on the plugins tab in ACT.  The order of names does not matter.
+
+The player name list can be exported and imported if necessary.
+
+## How it Works:
+Checks for seven total log lines that match the RegEx\
+`1B:[0-9A-F]{8}:[\\w'-]+ [\\w'-]+:[0-9A-F]{4}:[0-9A-F]{4}:00(14|29|77):[0-9A-F]{4}:[0-9A-F]{4}:[0-9A-F]{4}:`
+within 30 seconds of the first.
+
+## Problems:
+1. This doesn't account very well for when there's a single dead party member when Octet finishes casting.  You might get a false call in that case.
+2. There's probably a way to determine the party member names through the ACT API without having to have a fixed list, but I don't know how and don't want to spend the time to figure it out on my own.  If anyone knows how to easily do this, please let me know, and I will update the plugin.
